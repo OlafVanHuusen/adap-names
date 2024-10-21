e7afe9bd22108ad7a00a008afa6f57d312c7e727
OlafVanHuusen <lennard.clicque@fau.de>
2024-10-21 14:22:22 +0900

    Test changes

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -7,7 +7,8 @@ export class Name {
     private delimiter: string = this.DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        //Comment for git testing
+        throw new Error("needs implementation??");
     }
 
     public asNameString(delimiter: string = this.delimiter): string {

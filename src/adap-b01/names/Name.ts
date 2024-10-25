843d76dda25c568da98c41872b6e5d4e87b7af27
OlafVanHuusen <lennard.clicque@fau.de>
2024-10-25 13:22:55 +0700

    added method type annotations

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,6 +6,7 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
         this.components = other;
         if (delimiter) {
@@ -13,10 +14,12 @@ export class Name {
         }
     }
 
+    // @methodtype conversion-method
     public asNameString(delimiter: string = this.delimiter): string {
         return this.components.join(delimiter);
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
         if(i < 0 || i >= this.components.length) {
             throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
@@ -24,6 +27,7 @@ export class Name {
         return this.components[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         if(i < 0 || i >= this.components.length) {
             throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
@@ -31,11 +35,13 @@ export class Name {
         this.components[i] = c;
     }
 
+    // @methodtype get-method
     //getNumberOfComponents
     public getNoComponents(): number {
         return this.components.length;
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         if(i < 0 || i >= this.components.length) {
             throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
@@ -43,10 +49,12 @@ export class Name {
         this.components.splice(i, 0, c);
     }
 
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         if(i < 0 || i >= this.components.length) {
             throw new Error("Index: " + i + " out of bounds for length " + this.components.length);

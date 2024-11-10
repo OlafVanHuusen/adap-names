8a4afaa64dce34d5c6e613ec737a49e39d054dea
OlafVanHuusen <lennard.clicque@fau.de>
2024-11-11 00:26:49 +0100

    added homework 3

--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -7,52 +7,103 @@ export class StringName implements Name {
     protected name: string = "";
     protected length: number = 0;
 
+    protected regex: RegExp = new RegExp("(?<!\\\\)\\" + ".", 'g');
+
+    // @methodtype initialization-method
     constructor(other: string, delimiter?: string) {
-        throw new Error("needs implementation");
+        if(delimiter !== undefined) {
+            this.delimiter = delimiter;
+            this.regex = new RegExp("(?<!\\\\)\\" + this.delimiter, 'g');
+        }
+        this.name = other;
+        this.length = this.name.split(this.delimiter).length;
     }
 
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        if(delimiter === this.delimiter) {
+            return this.name;
+        }else {
+            return this.name.replace(ESCAPE_CHARACTER + this.delimiter, delimiter);
+        }
     }
 
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.name;
     }
 
+    // @methodtype boolean-query-method
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.length === 0;
     }
 
+    // @methodtype get-method
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.length;
     }
 
+    // @methodtype get-method
     public getComponent(x: number): string {
-        throw new Error("needs implementation");
+        if(x < 0 || x >= this.length) {
+            throw new Error("Index: " + x + " out of bounds for length " + this.length);
+        }
+        return this.name.split(this.regex)[x];
     }
 
+    // @methodtype set-method
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation");
+        if(n < 0 || n >= this.length) {
+            throw new Error("Index: " + n + " out of bounds for length " + this.length);
+        }
+        const nameArray = this.name.split(this.regex);
+        nameArray[n] = c.replace(this.delimiter, ESCAPE_CHARACTER + this.delimiter);
+        this.name = nameArray.join(this.delimiter);
     }
 
+    // @methodtype command-method
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation");
+        if(n < 0 || n >= this.length) {
+            throw new Error("Index: " + n + " out of bounds for length " + this.length);
+        }
+        const nameArray = this.name.split(this.regex);
+        nameArray.splice(n, 0, c.replace(this.delimiter, ESCAPE_CHARACTER + this.delimiter));
+        this.name = nameArray.join(this.delimiter);
+        this.length++;
     }
 
+    // @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.name += this.delimiter + c.replace(this.delimiter, ESCAPE_CHARACTER + this.delimiter);
+        this.length++;
     }
 
+    // @methodtype command-method
     public remove(n: number): void {
-        throw new Error("needs implementation");
+        if(n < 0 || n >= this.length) {
+            throw new Error("Index: " + n + " out of bounds for length " + this.length);
+        }
+        const nameArray = this.name.split(this.regex);
+        nameArray.splice(n, 1);
+        this.name = nameArray.join(this.delimiter);
+        this.length--;
     }
 
+    // @methodtype command-method
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        if(this.delimiter !== other.getDelimiterCharacter()) {
+            throw new Error("Can't concatenate names with different delimiters");
+        }
+        const temp = this.getNoComponents() + other.getNoComponents();
+        for(let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i).replace(this.delimiter, ESCAPE_CHARACTER + this.delimiter));
+        }
+        this.length = temp;
     }
 
 }

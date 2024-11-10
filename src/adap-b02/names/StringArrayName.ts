6a80e08761a2954f39d7f45b72b331356518c2fa
OlafVanHuusen <lennard.clicque@fau.de>
2024-11-11 00:26:49 +0100

    added homework 3

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -5,52 +5,84 @@ export class StringArrayName implements Name {
     protected components: string[] = [];
     protected delimiter: string = DEFAULT_DELIMITER;
 
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = other;
+        if (delimiter) {
+            this.delimiter = delimiter;
+        }
     }
 
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.map(x => x.replace(this.delimiter, ESCAPE_CHARACTER + this.delimiter)).join(this.delimiter);
     }
 
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    // @methodtype boolean-query-method
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.components.length === 0; 
     }
 
+    // @methodtype get-method
+    public getDelimiterCharacter(): string {
+        return this.delimiter;
+    }
+
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        if(i < 0 || i >= this.components.length) {
+            throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
+        }
+        return this.components[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        if(i < 0 || i >= this.components.length) {
+            throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
+        }
+        this.components[i] = c;
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        if(i < 0 || i >= this.components.length) {
+            throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
+        }
+        this.components.splice(i, 0, c);
     }
 
+    // @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        if(i < 0 || i >= this.components.length) {
+            throw new Error("Index: " + i + " out of bounds for length " + this.components.length);
+        }
+        this.components.splice(i, 1);
     }
 
+    // @methodtype command-method
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        if(this.delimiter !== other.getDelimiterCharacter()) {
+            throw new Error("Can't concatenate names with different delimiters");
+        }
+        for(let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
 
 }
